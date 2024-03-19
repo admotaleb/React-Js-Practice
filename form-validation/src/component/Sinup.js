f15c930ebcccbb3ae7775f784a85383bf86bd8dd
admotaleb <motalebmpicmt@gmail.com>
2024-03-19 14:47:38 +0600

    Update Sinup.js

--- form-validation/src/component/Sinup.js
+++ form-validation/src/component/Sinup.js
@@ -1,41 +1,48 @@
-import React, { useState } from 'react'
+import React from 'react'
+import * as yup from 'yup'
+import { useFormik } from 'formik'
 
 const Sinup = () => {
-    const [name, setName]=useState("");
-    const [email, setEmail]=useState("");
-    const [password, setPassword]=useState("");
-   const handelNameChange = (e) => {
-            setName(e.target.value);
-    };
-    
-    const handelEmailChange = (e) => {
-        setEmail(e.target.value);
-};
-const handelPasswordChange = (e) => {
-    setPassword(e.target.value);
-};
-const handelSubmit = (e) => {
-    e.preventDefault();
-    const newUser={
-        name, email, password
-    }
-    console.log(newUser);
-};
+    const formik = useFormik({
+      initialValues:{
+        name:"",
+        email:"",
+        password:""
+
+      },
+      validationSchema: yup.object({
+        name:yup.string().min(2,"Name must be 2 character").required(),
+        email:yup.string().min(2,"email must be 2 character").email().required(),
+        password:yup.string().min(6,"password must be 6 character").required()
+      }),
+      onSubmit:(values,{resetForm}) => {
+        console.log(values);
+        resetForm({
+          values:""
+        })
+      }
+       
+    })
+   console.error(formik.errors)
+
   return (
     <div>
         <h1>Sinup</h1>
-      <form onSubmit={handelSubmit}>
+      <form onSubmit={formik.handleSubmit}>
         <div>
             <label htmlFor='name'>Name : </label>
-            <input type="text" id="name" name='name' value={name} onChange={handelNameChange}/>
+            <input type="text" id="name" name='name' value={formik.values.name} onChange={formik.handleChange}/><br/>
+            {formik.touched.name && formik.errors.name && (<span style={{color:'red'}}>{formik.errors.name}</span>)}
         </div>
         <div>
-            <label htmlFor='email'>Name : </label>
-            <input type="text" id="email" name='email' value={email} onChange={handelEmailChange}/>
+            <label htmlFor='email'>Email : </label>
+            <input type="text" id="email" name='email' value={formik.values.email} onChange={formik.handleChange}/><br/>
+            {formik.touched.email && formik.errors.email && (<span style={{color:'red'}}>{formik.errors.email}</span>)}
         </div>
         <div>
-            <label htmlFor='password'>Name : </label>
-            <input type="text" id="password" name='password' value={password} onChange={handelPasswordChange}/>
+            <label htmlFor='password'>Password : </label>
+            <input type="text" id="password" name='password' value={formik.values.password} onChange={formik.handleChange}/><br/>
+            {formik.touched.password && formik.errors.password && (<span style={{color:'red'}}>{formik.errors.password}</span>)}
         </div>
         <button type='submit'>Submit</button>
       </form>
